b23ac8204adba9e5df172b46c1c89e5c3cae9343
Mohamed Awde <116069156+MohamedAwde@users.noreply.github.com>
2022-11-27 15:28:21 +0200

    add express

--- server.mjs
+++ server.mjs
@@ -1,6 +1,15 @@
-import { createServer } from 'http';
+import express from "express";
+import cors from "cors";
+const PORT = process.env.PORT | 8080;
+const app = express();
 
-createServer((req, res) => {
-  res.write('Hello World!');
-  res.end();
-}).listen(process.env.PORT);
+app.use(express());
+app.use(cors({ origin: "*" }));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/", async (_req, res) => {
+  res.send("the server is runing.");
+});
+
+app.listen(PORT, () => console.log(`server is listening on port ${PORT}`));
